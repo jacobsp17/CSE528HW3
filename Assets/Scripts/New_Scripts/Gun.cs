aa79c96c4d21ef78d0aa39e762d7172f2be55237
Jacob Price <j.s.price9@outlook.com>
2026-04-18 21:07:34 -0400

    final push

---
+++ Assets/Scripts/New_Scripts/Gun.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+
+    //bool for keeping track of gun's ability to fire
+    private bool readyToFire;
+    [SerializeField] private AudioClip gunShot;
+    private AudioSource audioSource;
+
+    void Start ()
+    {
+        readyToFire = true;
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        // Check if the left mouse button (0) is clicked AND the gun is ready
+        if (Input.GetMouseButtonDown(0) && readyToFire)
+        {
+            Bang();
+        }
+    }
+
+    public void Bang()
+    {
+        StartCoroutine(ShootAnim());
+    }
+
+    //accessor for checking gun's state
+    public bool ReadyToFire()
+    {
+        return readyToFire;
+    }
+
+    //animation for gun recoil; set readyToFire to false while animation plays
+    private IEnumerator ShootAnim()
+    {
+        if (audioSource != null)
+        {
+            audioSource.clip = gunShot;
+            audioSource.Play();
+        }
+            readyToFire = false;
+
+        //rotate in small increments every 1/30th of a second
+        for(int x = 0; x < 4; x++)
+        {
+            transform.Rotate(-12f, 0, 0);
+            yield return new WaitForSeconds(0.033f);
+        }
+        for (int i = 0; i < 4; i++)
+        {
+            transform.Rotate(12f, 0, 0);
+            yield return new WaitForSeconds(0.033f);
+        }
+        readyToFire = true;
+    }
+}
